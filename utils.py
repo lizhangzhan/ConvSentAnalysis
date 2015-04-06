3f9a360e33f946df5dc656f252979eec46b39f88
Mike Kestemont <mike.kestemont@gmail.com>
2015-04-06 16:22:08 +0200

    renamed to utils

--- utils.py
+++ utils.py
@@ -120,30 +120,3 @@ def vectorize(data, max_phrase_length):
     y = np.asarray(y, dtype=np.int8)
 
     return X, y
-
-
-#######################################################################################################################
-
-
-# get the data set
-data = get_data(n=10000000)
-
-
-# print the  first 10 data points
-for phrase, class_ in data[:50]:
-    print class_, phrase
-print
-print
-
-# print some statistics
-print_input_statistics(data)
-
-vocab_size = len(get_one_hot_vectors().keys())
-print "vocab size: "+str(vocab_size)
-
-print data[100][0]
-
-X, y = vectorize(data, 100)
-print X.shape
-
-# each char vector has length=267; we have 66 chars, so each phrase vector has length=267 * 67 = 17.889
